f25c39b34d762e8d6c04569a3ac20856fd3f7685
Gabriel Zacarias <gabriel.andrade@nicnet.com.br>
2023-09-12 14:26:31 -0300

    5 lances por leilao

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -20,7 +20,42 @@ class Leilao
         if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
             return;
         }
+
+        $usuario = $lance->getUsuario();
+        $totalLancesUsuario = array_reduce(
+            $this->lances,
+            function(int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },
+            0
+        );
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return Lance|mixed
+     */
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLancesUsuario = array_reduce(
+            $this->lances,
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },
+            0
+        );
+        return $totalLancesUsuario;
     }
 
     /**
